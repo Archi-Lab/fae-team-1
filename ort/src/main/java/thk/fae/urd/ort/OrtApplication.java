1b1d6582a2d449386e53b9abdb1baf3bafe7dd06
Dennis <nitro_123@web.de>
2018-12-21 11:55:18 +0100

    Simple kafka consumer integration

--- ort/src/main/java/thk/fae/urd/ort/OrtApplication.java
+++ ort/src/main/java/thk/fae/urd/ort/OrtApplication.java
@@ -1,12 +1,19 @@
 package thk.fae.urd.ort;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class OrtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrtApplication.class, args);
+	}
+
+	@KafkaListener(topics = "tracker")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+		System.out.println(cr.toString());
 	}
 }
